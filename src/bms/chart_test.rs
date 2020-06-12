1ed0e21e5f1b89c1c6c13d64a6e99da833922dfc
tpdns90321 <tpdns9032100@gmail.com>
2020-06-12 22:01:18 +0900

    [init] Chart의 alphanum 버젼 test도 추가했다.
    1. Alphanum 버젼의 test 코드 추가
    2. Alphanum 최고 숫자를 넣을려고 의도한 숫자가 아닌 오타 수정
    3. 주석 추가

--- src/bms/chart_test.rs
+++ src/bms/chart_test.rs
@@ -3,7 +3,7 @@ use super::chart::{Chart, ChartType};
 const TEST_CHART: &'static str = "
 #00003:DE
 #00002:0.5
-#00011:00010002
+#00011:00zz0002
 #ZZZ11:00100000";
 
 #[test]
@@ -11,7 +11,21 @@ fn test_chart_hex_struct() {
 	let chart = Chart::parse(ChartType::Hex, TEST_CHART).unwrap();
 
 	assert_eq!(*chart.melodies().get(&0).unwrap().length(), 0.5);
+	// FREEDOM DIVE BPM: 222
 	assert_eq!(*chart.melodies().get(&0).unwrap().bpm(), vec![222]);
 	// out of range test
-	assert!(chart.melodies().get(&4095).is_none());
+	// alphanum maximum range 46655
+	assert!(chart.melodies().get(&46655).is_none());
+}
+
+#[test]
+fn test_chart_alphanum_struct() {
+	let chart = Chart::parse(ChartType::Alphanum, TEST_CHART).unwrap();
+
+	assert_eq!(*chart.melodies().get(&0).unwrap().length(), 0.5);
+	// FREEDOM DIVE BPM: 222
+	assert_eq!(*chart.melodies().get(&0).unwrap().bpm(), vec![222]);
+	// alphanum range test
+	// alphanum maximum range 46655
+	assert!(chart.melodies().get(&46655).is_some());
 }
