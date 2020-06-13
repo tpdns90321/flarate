7bc52989cd69bd48adfa27d0035cfd6d4600bdfc
tpdns90321 <tpdns9032100@gmail.com>
2020-06-13 11:34:50 +0900

    [init] alphanumeric 모드와 hex 차이점을 부각
    테스트 코드가 서로 비슷한데 사소한 부분에서라도 alphanumeric 모드와
    hex 모드와의 차이점을 부각하기 위해 마디 object 채널 테스트 부분의
    마디 번호를 A0으로 설정 후 변한 값들을 반영했습니다.

--- src/bms/chart_test.rs
+++ src/bms/chart_test.rs
@@ -3,7 +3,7 @@ use super::chart::{Chart, ChartType};
 const TEST_CHART: &'static str = "
 #00003:DE
 #00002:0.5
-#00011:00zz0002
+#0A011:00zz0002
 #ZZZ11:00100000";
 
 #[test]
@@ -15,7 +15,7 @@ fn test_chart_hex_struct() {
 	assert_eq!(*chart.melodies().get(&0).unwrap().bpm(), vec![222]);
 	// Object Channel Chart
 	assert_eq!(
-		*chart.melodies().get(&0).unwrap().first_player_visible().get(&1).unwrap(),
+		*chart.melodies().get(&160).unwrap().first_player_visible().get(&1).unwrap(),
 		vec![0,1295,0,02]
 	);
 	// out of range test
@@ -32,7 +32,7 @@ fn test_chart_alphanum_struct() {
 	assert_eq!(*chart.melodies().get(&0).unwrap().bpm(), vec![222]);
 	// Object Channel Chart
 	assert_eq!(
-		*chart.melodies().get(&0).unwrap().first_player_visible().get(&1).unwrap(),
+		*chart.melodies().get(&360).unwrap().first_player_visible().get(&1).unwrap(),
 		vec![0,1295,0,02]
 	);
 	// alphanum range test
